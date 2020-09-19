c1a1137b3727d80f8b05ece49ea1fb94331b6162
Saksham Sharma <saksham.sh17@gmail.com>
2020-09-19 14:19:33 +0530

    Create pattern6.java

---
+++ pattern6.java
@@ -0,0 +1,29 @@
+//    * 
+//   * * 
+//  * * * 
+// * * * * 
+//* * * * * 
+package loops;
+
+import java.util.Scanner;
+
+public class Pattern4 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i = 1 ; i<= n ; i++) {
+			/// to print spaces
+				for (int j =1; j <= n-i  ; j++) {
+					System.out.print(" ");
+				}
+				/// to print stars
+				for (int j = 1; j <= i ; j++) {
+					System.out.print("* ");
+				}
+				System.out.println();
+			}
+		}
+
+}
+
